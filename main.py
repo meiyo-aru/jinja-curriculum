aa0022638db78bb71922fea39e289164d6f45b2f
blink992 <alleybossmangas@gmail.com>
2025-07-11 23:17:47 -0300

    correcao do tratamento da resposta das requisicoes

--- main.py
+++ main.py
@@ -17,38 +17,38 @@ async def head_root():
 
 @app.get("/get/about_me")
 async def get_about_me(people_id: int = 1, language: str = "pt"):
-    response = requests.get(url_api + "/get/people?people_id=" + people_id)
+    response = requests.get(url_api + "/get/people?people_id=" + people_id).json()
     about_me = env.get_template("about_me.html")
     
     if language == "pt":
         html = about_me.render(title_about_me="Sobre mim", 
-                               about_me=response.about)
+                               about_me=response.get("about"))
     else:
         html = about_me.render(title_about_me="About me", 
-                               about_me=translator.translate(response.about, dest=language))
+                               about_me=translator.translate(response.get("about"), dest=language))
         
     return html
 
 @app.get("/get/header")
-async def get_about_me(people_id: int = 1, language: str = "pt"):
-    response = requests.get(url_api + "/get/people?people_id=" + people_id)
+async def get_header(people_id: int = 1, language: str = "pt"):
+    response = requests.get(url_api + "/get/people?people_id=" + people_id).json()
     header = env.get_template("header.html")
     
     if language == "pt":
-        html = header.render(linkedin=response.linkedin, 
-                             mail=response.mail, 
-                             phone=response.phone, 
-                             address=response.address, 
-                             positions=response.positions, 
-                             name=response.name,
+        html = header.render(linkedin=response.get("linkedin"), 
+                             mail=response.get("mail"), 
+                             phone=response.get("phone"), 
+                             address=response.get("address"), 
+                             positions=response.get("positions"), 
+                             name=response.get("name"),
                              profile_title="Perfil")
     else:
-        html = header.render(linkedin=response.linkedin, 
-                             mail=response.mail, 
-                             phone=response.phone, 
-                             address=response.address, 
-                             positions=response.positions, 
-                             name=response.name,
+        html = header.render(linkedin=response.get("linkedin"), 
+                             mail=response.get("mail"), 
+                             phone=response.get("phone"), 
+                             address=response.get("address"), 
+                             positions=response.get("positions"), 
+                             name=response.get("name"),
                              profile_title=translator.translate("Perfil", dest=language))
         
     return html
